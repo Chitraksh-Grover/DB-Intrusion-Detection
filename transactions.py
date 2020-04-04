6bc1899b660650bf947d3e9f965132e730f96bf3
Chitraksh Grover <chitrakshgrover@gmail.com>
2020-04-04 18:53:47 +0530

    securitydetail complete

--- transactions.py
+++ transactions.py
@@ -15,9 +15,8 @@ def createProfile(profile,trans):
             URV[dg.URV_feature_index[feature] + 1] = len(profile[feature])
         else:
             try:
-                URV[dg.URV_feature_index[feature]] = statistics.mean(profile[feature])
-                URV[dg.URV_feature_index[feature]+1] = statistics.median(profile[feature])
-                URV[dg.URV_feature_index[feature]+2] = statistics.stdev(profile[feature])
+                URV[dg.URV_feature_index[feature]] = np.mean(profile[feature])
+                URV[dg.URV_feature_index[feature]+1] = np.median(profile[feature])
                 URV[dg.URV_feature_index[feature]+3] = max(profile[feature])
                 URV[dg.URV_feature_index[feature]+4] = min(profile[feature])
             except:
@@ -260,8 +259,15 @@ def marketwatch(acct_id=0,cust_id=0,industry_name='',ending_co_id=0,starting_co_
         security = dg.Security.loc[dg.Security['S_CO_ID'].isin(company_id),['S_CO_ID','S_SYMB']]
         stock_list = list(security['S_SYMB'])
         profile['S_CO_ID'] = list(security['S_CO_ID'])
-        profile['S_SYMB'] = stock_list
-        
+    
+    profile['S_SYMB'] = stock_list
+    profile['LT_PRICE'] = []
+    profile['LT_S_SYMB'] = stock_list
+    profile['S_NUM_OUT'] = []
+    profile['DM_DATE'] = []
+    profile['DM_S_SYMB'] = stock_list
+    profile['DM_CLOSE'] = []
+    
     old_mkt_cap = 0.00
     new_mkt_cap = 0.00
     pct_change = 0.00
@@ -279,9 +285,174 @@ def marketwatch(acct_id=0,cust_id=0,industry_name='',ending_co_id=0,starting_co_
             old_price = dg.DailyMarket.loc[(dg.DailyMarket['DM_DATE'] == start_date) & (dg.DailyMarket['DM_S_SYMB'] == symbol),'DM_CLOSE'].values[0]
             old_mkt_cap += s_num_out * old_price
             new_mkt_cap += s_num_out * new_price
+            profile['LT_PRICE'].append(new_price)
+            profile['S_NUM_OUT'].append(s_num_out)
+            profile['DM_DATE'].append(start_date)
+            profile['DM_CLOSE'].append(old_price)
         except:
             old_mkt_cap = 0.00
     if old_mkt_cap != 0.00:
         pct_change = 100 * (new_mkt_cap / old_mkt_cap - 1)
-    
+        
+    createProfile(profile, ' marketwatch ' + str(cust_id) + '' + str(acct_id) )
     return acct_id,cust_id,industry_name,ending_co_id,starting_co_id,start_date,pct_change
+
+############################SECURITY DETAIL TRANSACTION########################
+def securitydetail(access_lob_flag = '',max_rows_to_return = '',start_day = '',symbol = ''):
+    profile = {}
+    if symbol == '':
+        security = list(dg.Security['S_SYMB'])
+        symbol = security[np.random.randint(len(security))]
+    security = dg.Security.loc[dg.Security['S_SYMB'] == symbol,['S_CO_ID',
+   'S_NAME','S_NUM_OUT','S_START_DATE','S_EXCH_DATE','S_PE','S_52WK_HIGH',
+   'S_52WK_HIGH_DATE','S_52WK_LOW','S_52WK_LOW_DATE','S_DIVIDEND','S_YIELD','S_EX_ID']]
+    profile['S_SYMB'] = [symbol]
+    profile['S_CO_ID'] = [security['S_CO_ID'].values[0]]
+    profile['S_NAME'] = [security['S_NAME'].values[0]]
+    profile['S_NUM_OUT'] = [security['S_NUM_OUT'].values[0]]
+    profile['S_START_DATE'] = [security['S_START_DATE'].values[0]]
+    profile['S_EXCH_DATE'] = [security['S_EXCH_DATE'].values[0]]
+    profile['S_PE'] = [security['S_PE'].values[0]]
+    profile['S_52WK_HIGH'] = [security['S_52WK_HIGH'].values[0]]
+    profile['S_52WK_HIGH_DATE'] = [security['S_52WK_HIGH_DATE'].values[0]]
+    profile['S_52WK_LOW'] = [security['S_52WK_LOW'].values[0]]
+    profile['S_52WK_LOW_DATE'] = [security['S_52WK_LOW_DATE'].values[0]]
+    profile['S_DIVIDEND'] = [security['S_DIVIDEND'].values[0]]
+    profile['S_YIELD'] = [security['S_YIELD'].values[0]]
+    profile['S_EX_ID'] = [security['S_EX_ID'].values[0]]
+    company = dg.Company.loc[dg.Company['CO_ID'] == int(security['S_CO_ID']),[
+    'CO_ID','CO_AD_ID','CO_NAME','CO_SP_RATE','CO_CEO','CO_DESC','CO_OPEN_DATE',
+    'CO_ST_ID']]
+    profile['CO_ID'] = [company['CO_ID'].values[0]]
+    profile['CO_AD_ID'] = [company['CO_AD_ID'].values[0]]
+    profile['CO_NAME'] = [company['CO_NAME'].values[0]]
+    profile['CO_SP_RATE'] = [company['CO_SP_RATE'].values[0]]
+    profile['CO_CEO'] = [company['CO_CEO'].values[0]]
+    profile['CO_DESC'] = [company['CO_DESC'].values[0]]
+    profile['CO_OPEN_DATE'] = [company['CO_OPEN_DATE'].values[0]]
+    profile['CO_ST_ID'] = [company['CO_ST_ID'].values[0]]
+    exchange = dg.Exchange.loc[dg.Exchange['EX_ID'] == security['S_EX_ID'].values[0],
+    ['EX_AD_ID','EX_CLOSE','EX_DESC','EX_NAME','EX_NUM_SYMB','EX_OPEN']]
+    profile['EX_ID'] = [security['S_EX_ID'].values[0]]
+    profile['EX_AD_ID'] = [exchange['EX_AD_ID'].values[0]]
+    profile['EX_CLOSE'] = [exchange['EX_CLOSE'].values[0]]
+    profile['EX_DESC'] = [exchange['EX_DESC'].values[0]]
+    profile['EX_NAME'] = [exchange['EX_NAME'].values[0]]
+    profile['EX_NUM_SYMB'] = [exchange['EX_NUM_SYMB'].values[0]]
+    profile['EX_OPEN'] = [exchange['EX_OPEN'].values[0]]
+    ca_address = dg.Address.loc[dg.Address['AD_ID'] == company['CO_AD_ID'].values[0],
+    ["AD_ID", "AD_LINE1","AD_LINE2","AD_ZC_CODE","AD_CTRY"]]
+    profile["AD_ID"] = [ca_address["AD_ID"].values[0]]
+    profile["AD_LINE1"] = [ca_address["AD_LINE1"].values[0]]
+    profile["AD_LINE2"] = [ca_address["AD_LINE2"].values[0]]
+    profile["AD_ZC_CODE"] = [ca_address["AD_ZC_CODE"].values[0]]
+    profile["AD_CTRY"] = [ca_address["AD_CTRY"].values[0]]
+    ea_address = dg.Address.loc[dg.Address['AD_ID'] == exchange['EX_AD_ID'].values[0],
+    ["AD_ID", "AD_LINE1","AD_LINE2","AD_ZC_CODE","AD_CTRY"]]
+    profile["AD_ID"].append(ea_address["AD_ID"].values[0])
+    profile["AD_LINE1"].append(ea_address["AD_LINE1"].values[0])
+    profile["AD_LINE2"].append(ea_address["AD_LINE2"].values[0])
+    profile["AD_ZC_CODE"].append(ea_address["AD_ZC_CODE"].values[0])
+    profile["AD_CTRY"].append(ea_address["AD_CTRY"].values[0])
+    zca = dg.ZipCode.loc[dg.ZipCode['ZC_CODE'] == ca_address['AD_ZC_CODE'].values[0],
+    ["ZC_CODE", "ZC_TOWN","ZC_DIV"]]
+    profile["ZC_CODE"] = [zca["ZC_CODE"].values[0]]
+    profile["ZC_TOWN"] = [zca["ZC_TOWN"].values[0]]
+    profile["ZC_DIV"] = [zca["ZC_DIV"].values[0]]
+    zea = dg.ZipCode.loc[dg.ZipCode['ZC_CODE'] == ea_address['AD_ZC_CODE'].values[0],
+    ["ZC_CODE", "ZC_TOWN","ZC_DIV"]]
+    profile["ZC_CODE"].append(zea["ZC_CODE"].values[0])
+    profile["ZC_TOWN"].append(zea["ZC_TOWN"].values[0])
+    profile["ZC_DIV"].append(zea["ZC_DIV"].values[0])
+    competitor = dg.CompanyCompetitor.loc[dg.CompanyCompetitor['CP_CO_ID'] == 
+    company['CO_ID'].values[0],["CP_CO_ID", "CP_COMP_CO_ID","CP_IN_ID"]]
+    profile["CP_CO_ID"] = list(competitor["CP_CO_ID"].values)
+    profile["CP_COMP_CO_ID"] = list(competitor["CP_COMP_CO_ID"].values)
+    profile["CP_IN_ID"] = list(competitor["CP_IN_ID"].values)
+    cp_co_name = []
+    cp_in_name = []
+    for row in competitor.iterrows():
+        co_name = dg.Company.loc[dg.Company['CO_ID'] == row[1]['CP_COMP_CO_ID'],'CO_NAME'].values[0]
+        in_name = dg.Industry.loc[dg.Industry['IN_ID'] == row[1]['CP_IN_ID'],'IN_NAME'].values[0]
+        cp_co_name.append(co_name)
+        cp_in_name.append(in_name)
+    profile['CO_ID'].extend(list(competitor["CP_COMP_CO_ID"].values))
+    profile['CO_NAME'] = cp_co_name
+    profile['IN_ID'] = list(competitor["CP_IN_ID"].values)
+    profile['IN_NAME'] = cp_in_name
+    financial = dg.Financial.loc[dg.Financial['FI_CO_ID']==company['CO_ID'].values[0],
+    ["FI_CO_ID","FI_YEAR","FI_QTR","FI_OTR_START_DATE","FI_REVENUE","FI_NET_EARN",
+    "FI_BASIC_EPS","FI_DILUT_EPS","FI_MARGIN","FI_INVENTORY","FI_ASSETS","FI_LIABILITY"
+    ,"FI_OUT_BASIC","FI_OUT_DILUT"]]
+    financial = financial[:20]
+    profile["FI_CO_ID" ]= list(financial["FI_CO_ID"].values)
+    profile["FI_YEAR"] = list(financial["FI_YEAR"].values)
+    profile["FI_QTR"] = list(financial["FI_QTR"].values)
+    profile["FI_OTR_START_DATE"] = list(financial["FI_OTR_START_DATE"].values)
+    profile["FI_REVENUE"] = list(financial["FI_REVENUE"].values)
+    profile["FI_NET_EARN"] = list(financial["FI_NET_EARN"].values)
+    profile["FI_BASIC_EPS"] = list(financial["FI_BASIC_EPS"].values)
+    profile["FI_DILUT_EPS"] = list(financial["FI_DILUT_EPS"].values)
+    profile["FI_MARGIN"] = list(financial["FI_MARGIN"].values)
+    profile["FI_INVENTORY"] = list(financial["FI_INVENTORY"].values)
+    profile["FI_ASSETS"] = list(financial["FI_ASSETS"].values)
+    profile["FI_LIABILITY"] = list(financial["FI_LIABILITY"].values)
+    profile["FI_OUT_BASIC"] = list(financial["FI_OUT_BASIC"].values)
+    profile["FI_OUT_DILUT"] = list(financial["FI_OUT_DILUT"].values)
+    if max_rows_to_return == '':
+        max_rows_to_return = np.random.randint(5,21)
+    if start_day == '':
+        date = list(dg.DailyMarket['DM_DATE'])
+        start_day = date[np.random.randint(len(date)-max_rows_to_return)]
+    dailymarket = dg.DailyMarket.loc[(dg.DailyMarket['DM_S_SYMB'] == symbol) 
+    & (dg.DailyMarket['DM_DATE']>=start_day),['DM_S_SYMB','DM_DATE','DM_CLOSE','DM_HIGH',
+   'DM_LOW','DM_VOL']].head(max_rows_to_return)
+    profile['DM_S_SYMB'] = list(dailymarket['DM_S_SYMB'].values)
+    profile['DM_DATE'] = list(dailymarket['DM_DATE'].values)
+    profile['DM_CLOSE'] = list(dailymarket['DM_CLOSE'].values)
+    profile['DM_HIGH'] = list(dailymarket['DM_HIGH'].values)
+    profile['DM_LOW'] = list(dailymarket['DM_LOW'].values)
+    profile['DM_VOL'] = list(dailymarket['DM_VOL'].values)
+    lasttrade = dg.LastTrade.loc[dg.LastTrade['LT_S_SYMB']==symbol,['LT_S_SYMB','LT_PRICE',
+    'LT_OPEN_PRICE','LT_VOL']]
+    profile['LT_S_SYMB'] = list(lasttrade['LT_S_SYMB'].values)
+    profile['LT_PRICE'] = list(lasttrade['LT_PRICE'].values)
+    profile['LT_OPEN_PRICE'] = list(lasttrade['LT_OPEN_PRICE'].values)
+    profile['LT_VOL'] = list(lasttrade['LT_VOL'].values)
+    if access_lob_flag == '':
+        access_lob_flag = np.random.randint(2)
+    if access_lob_flag == 1:
+        newsxref = dg.NewsXRef.loc[dg.NewsXRef['NX_CO_ID'] == company['CO_ID'].values[0],
+        ['NX_CO_ID','NX_NI_ID']].head(2)
+        profile['NX_CO_ID'] = list(newsxref['NX_CO_ID'].values)
+        profile['NX_NI_ID'] = list(newsxref['NX_NI_ID'].values)
+        newsitem = dg.NewsItem.loc[dg.NewsItem['NI_ID'].isin(list(newsxref['NX_NI_ID'])),
+        ['NI_ID','NI_ITEM','NI_DTS','NI_SOURCE','NI_AUTHOR']]
+        profile['NI_ID'] = list(newsitem['NI_ID'].values)
+        profile['NI_ITEM'] = list(newsitem['NI_ITEM'].values)
+        profile['NI_DTS'] = list(newsitem['NI_DTS'].values)
+        profile['NI_SOURCE'] = list(newsitem['NI_SOURCE'].values)
+        profile['NI_AUTHOR'] = list(newsitem['NI_AUTHOR'].values)
+    else:
+        newsxref = dg.NewsXRef.loc[dg.NewsXRef['NX_CO_ID'] == company['CO_ID'].values[0],
+        ['NX_CO_ID','NX_NI_ID']].head(2)
+        profile['NX_CO_ID'] = list(newsxref['NX_CO_ID'].values)
+        profile['NX_NI_ID'] = list(newsxref['NX_NI_ID'].values)
+        newsitem = dg.NewsItem.loc[dg.NewsItem['NI_ID'].isin(list(newsxref['NX_NI_ID'])),
+        ['NI_ID','NI_DTS','NI_SOURCE','NI_AUTHOR','NI_HEADLINE','NI_SUMMARY']]
+        profile['NI_ID'] = list(newsitem['NI_ID'].values)
+        profile['NI_DTS'] = list(newsitem['NI_DTS'].values)
+        profile['NI_SOURCE'] = list(newsitem['NI_SOURCE'].values)
+        profile['NI_AUTHOR'] = list(newsitem['NI_AUTHOR'].values)
+        profile['NI_HEADLINE'] = list(newsitem['NI_HEADLINE'].values)
+        profile['NI_SUMMARY'] = list(newsitem['NI_SUMMARY'].values)
+        
+    createProfile(profile, " securitydetail")
+    
+    return access_lob_flag,start_day,symbol,max_rows_to_return,security,company,
+    exchange,ca_address,ea_address,zca,zea,cp_co_name,cp_in_name,financial,dailymarket,
+    lasttrade,newsitem
+
+  
+
+    
